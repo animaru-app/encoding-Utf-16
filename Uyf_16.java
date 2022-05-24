1a40276ac5cbb3e57bbd0bcad83b3595167fa986
Max <100166330+Nzoth665@users.noreply.github.com>
2022-05-24 15:18:36 +0300

    Update Uyf_16.java

--- Uyf_16.java
+++ Uyf_16.java
@@ -6,23 +6,6 @@ public class Uyf_16 {
         for (int i=0;cis<128;cis++,boks++){
             Uyf[cis] = ""+boks;
             System.out.println(cis+"-"+boks);
-        }
+        };
     };
-    
-    
-
-    
-    /*
-    Uyf_17 test2 = new Uyf_17();
-    return test2.test1();
-    */
-    
 };
-
-/*class Uyf_17 {
-    public int test1(){
-        return 1;
-    }
-    
-}
-*/
