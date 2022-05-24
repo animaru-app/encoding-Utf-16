a819d953e8b5f59317c6ee8812fe3ac69d07b3d3
Max <100166330+Nzoth665@users.noreply.github.com>
2022-05-24 15:17:45 +0300

    Utf-16 encoding table code

---
+++ Uyf_16.java
@@ -0,0 +1,28 @@
+public class Uyf_16 {
+    public static void main(String[] args){
+        String Uyf[] = new String [127];
+        int cis=0;
+        char boks=0;
+        for (int i=0;cis<128;cis++,boks++){
+            Uyf[cis] = ""+boks;
+            System.out.println(cis+"-"+boks);
+        }
+    };
+    
+    
+
+    
+    /*
+    Uyf_17 test2 = new Uyf_17();
+    return test2.test1();
+    */
+    
+};
+
+/*class Uyf_17 {
+    public int test1(){
+        return 1;
+    }
+    
+}
+*/
